832e069004af0fa5e4ef3b4391828f5951cd6fe5
lucas baleeiro <lucasdominato2@gmail.com>
2021-07-04 17:05:37 -0300

    updated rpc code

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	client "Network-and-System-Distributed-studies/rpc/rpc-client"
 	server "Network-and-System-Distributed-studies/rpc/rpc-server"
-	"time"
 )
 
 func main() {
 
 	// running rpc server
-	go server.RpcServer("127.0.0.1:8081")
+	server.RpcServer("127.0.0.1:8081")
 
-	time.Sleep(5 * time.Second)
+	// time.Sleep(5 * time.Second)
 
 	// running rpc client
 	client.RpcClient("127.0.0.1:8081", 4, 2)
