4eedaef2e3d288b63eb230fa0ca00be9db610714
lucas baleeiro <lucasdominato2@gmail.com>
2021-07-11 22:44:05 -0300

    solved bug in rpc-chat code

--- cmd/main.go
+++ cmd/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	client "Network-and-Distributed-systems-studies/chat-rpc/chat-client"
+	server "Network-and-Distributed-systems-studies/chat-rpc/chat-server"
 )
 
 func main() {
 
 	// running rpc server
-	// server.ChatServer(":8081")
+	server.ChatServer(":8081")
 
 	// time.Sleep(5 * time.Second)
 
-	client.ChatClient("127.0.0.1:8081", "gabriel")
+	client.ChatClient(":8081", "gabriel")
 
 }
